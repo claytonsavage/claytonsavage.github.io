83d67f6f73e5426876f6bf28a0d8715c55267064
claytonsavage <clytnsavage@gmail.com>
2018-02-27 19:29:32 -0800

    JavaScript moved into index.html
    put js in index

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-window.onload = function () {
+// window.onload = function () {
 
-    var numselection = document.getElementsByClassName("selection").length;
+//     var numselection = document.getElementsByClassName("selection").length;
 
-    for (var i = 0; i < numselection; i++) {
-        document.getElementsByClassName("selection")[i].addEventListener("mouseover", function(event) {
-           var name = document.getElementsByClassName("project-individual")[(this.innerHTML*1-1)+6].innerHTML;
-           var newName = document.getElementsByClassName(name)[0].className.concat(" hide");
-           document.getElementsByClassName(name)[0].className = newName;
-           var backname = String(name).trim().concat("back");
-           var newBackName = document.getElementsByClassName(backname)[0].className.concat(" show");
-           document.getElementsByClassName(backname)[0].className = newBackName
-        })
-        document.getElementsByClassName("selection")[i].addEventListener("mouseleave", function(event) {
-            var name = document.getElementsByClassName("project-individual")[(this.innerHTML*1-1)+6].innerHTML;
-            var newName = document.getElementsByClassName(name)[0].className.slice(0,-5);
-            document.getElementsByClassName(name)[0].className = newName;
-            var backname = String(name).trim().concat("back");
-            document.getElementsByClassName(backname)[0].className = "project-individual ".concat(backname);
-         })
-    }
-}
+//     for (var i = 0; i < numselection; i++) {
+//         document.getElementsByClassName("selection")[i].addEventListener("mouseover", function(event) {
+//            var name = document.getElementsByClassName("project-individual")[(this.innerHTML*1-1)+6].innerHTML;
+//            var newName = document.getElementsByClassName(name)[0].className.concat(" hide");
+//            document.getElementsByClassName(name)[0].className = newName;
+//            var backname = String(name).trim().concat("back");
+//            var newBackName = document.getElementsByClassName(backname)[0].className.concat(" show");
+//            document.getElementsByClassName(backname)[0].className = newBackName
+//         })
+//         document.getElementsByClassName("selection")[i].addEventListener("mouseleave", function(event) {
+//             var name = document.getElementsByClassName("project-individual")[(this.innerHTML*1-1)+6].innerHTML;
+//             var newName = document.getElementsByClassName(name)[0].className.slice(0,-5);
+//             document.getElementsByClassName(name)[0].className = newName;
+//             var backname = String(name).trim().concat("back");
+//             document.getElementsByClassName(backname)[0].className = "project-individual ".concat(backname);
+//          })
+//     }
+// }
